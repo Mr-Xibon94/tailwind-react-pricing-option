dc0a42e4bfcd069e1d05285e503ce632a27b7bd3
Mr-Xibon94 <md19jibon94@gmail.com>
2026-03-24 14:10:55 +0600

    added button

--- src/components/Pricingoptions/PriceCards.jsx
+++ src/components/Pricingoptions/PriceCards.jsx
@@ -6,17 +6,18 @@ const PriceCards = ({ priceCard }) => {
 
     const {name, price, duration,features } =priceCard;
     return (
-        <div className='border rounded-2xl bg-cyan-200 m-4 text-black p-4'>
+        <div className='flex flex-col border rounded-2xl bg-cyan-200 m-4 text-black p-4'>
             <div className='text-center'>
                 <h1 className='text-3xl font-semibold'>{name}</h1>
-                <h2 className='text-2xl font-medium'>{price}/{duration}</h2>
+                <h2 className='text-2xl font-medium'>${price}/{duration}</h2>
             </div>
             <h2 className='text-center text-2xl'></h2>
-            <div className='bg-cyan-300 m-6 p-6 space-y-2.5 rounded-2xl'>
+            <div className='bg-cyan-300 m-6 p-6 space-y-2.5 rounded-2xl flex-1'>
                 {
                     features.map((feature, index)=><Features feature={feature}></Features>)
                 }
             </div>
+            <button className="btn  w-full">Subscribe</button>
 
 
         </div>
